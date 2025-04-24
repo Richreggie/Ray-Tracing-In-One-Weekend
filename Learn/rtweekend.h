e7e1e743276956020e25e6f47ce3ca09c5cd8458
Richreggie <2326883616@qq.com>
2025-04-24 10:57:47 +0800

    抗锯齿

--- Learn/rtweekend.h
+++ Learn/rtweekend.h
@@ -3,6 +3,7 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>//随机数工具
 #include <limits>
 #include <memory>
 
@@ -24,7 +25,17 @@ inline double degrees_to_radians(double degrees) {     // inline 建议编译器
     return degrees * pi / 180.0;
 }
 
-// Common Headers
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_double();
+}
+
+// Common Headers 
 
 #include "ray.h"
 #include "vec3.h"

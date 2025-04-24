ea977a4be0e8cd6bf7c517df41540a5df84b38ab
Richreggie <2326883616@qq.com>
2025-04-24 21:00:04 +0800

    调试相机（视场，定位与定向，失焦模糊）

--- Learn/camera.h
+++ Learn/camera.h
@@ -18,6 +18,12 @@ public:
     int    max_depth = 10;   // Maximum number of ray bounces into scene 初始光线 → 碰撞物体 → 生成散射光线 → 递归追踪新光线 → 未碰撞 → 返回背景色
     
     double vfov = 90;  // 垂直视角（视场）
+    point3 lookfrom = point3(0, 0, -1);  // 相机查看的位置
+    point3 lookat = point3(0, 0, 0);   // 相机正在看的点
+    vec3   vup = vec3(0, 1, 0);     // 相机相对的“向上”方向
+
+    double defocus_angle = 0;  // 通过每个像素的光线的变化角度
+    double focus_dist = 10;    // 从相机观察点到完美对焦平面的距离
 
     void render(const hittable& world) {
         initialize();
@@ -47,41 +53,53 @@ private:
     point3 pixel00_loc;    // Location of pixel 0, 0
     vec3   pixel_delta_u;  // Offset to pixel to the right
     vec3   pixel_delta_v;  // Offset to pixel below
+    vec3   u, v, w;
+    vec3   defocus_disk_u;  // 失焦盘的水平半径
+    vec3   defocus_disk_v;  // 失焦盘的垂直半径
 
     void initialize() {
         image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
 
-        center = point3(0, 0, 0);
+        center = lookfrom;
 
         // Determine viewport dimensions.
-        auto focal_length = 1.0;
+        //auto focal_length = (lookfrom - lookat).length();
         auto theta = degrees_to_radians(vfov);
         auto h = tan(theta / 2);
-        auto viewport_height = 2 * h * focal_length;
+        auto viewport_height = 2 * h * focus_dist;
         auto viewport_width = viewport_height * (double(image_width) / image_height);
 
-        // Calculate the vectors across the horizontal and down the vertical viewport edges.
-        auto viewport_u = vec3(viewport_width, 0, 0);
-        auto viewport_v = vec3(0, -viewport_height, 0);
+        // 计算相机坐标系的 u,v,w 单位基向量。
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
+
+        // 计算沿视口水平和垂直边缘的向量。
+        vec3 viewport_u = viewport_width * u;    // 视口水平边缘的向量
+        vec3 viewport_v = viewport_height * -v;  // 视口垂直边缘的向量
 
         // Calculate the horizontal and vertical delta vectors from pixel to pixel.
         pixel_delta_u = viewport_u / image_width;
         pixel_delta_v = viewport_v / image_height;
 
         // Calculate the location of the upper left pixel.
-        auto viewport_upper_left =
-            center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+        auto viewport_upper_left = center - (focus_dist * w) - viewport_u / 2 - viewport_v / 2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        // 计算相机失焦盘的基向量。
+        auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle / 2));
+        defocus_disk_u = u * defocus_radius;
+        defocus_disk_v = v * defocus_radius;
     }
 
     ray get_ray(int i, int j) const {
-        // Get a randomly sampled camera ray for the pixel at location i,j.
-
+        //// Get a randomly sampled camera ray for the pixel at location i,j.
+        // 获取像素位置i,j的随机采样相机光线，起源于相机的失焦盘。
         auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
         auto pixel_sample = pixel_center + pixel_sample_square();
 
-        auto ray_origin = center;
+        auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
 
         return ray(ray_origin, ray_direction);
@@ -92,6 +110,12 @@ private:
         auto px = -0.5 + random_double();
         auto py = -0.5 + random_double();
         return (px * pixel_delta_u) + (py * pixel_delta_v);
+    }
+
+    point3 defocus_disk_sample() const {
+        // 返回相机失焦盘中的一个随机点。
+        auto p = random_in_unit_disk();
+        return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
     }
 
     color ray_color(const ray& r, int depth, const hittable& world) const {

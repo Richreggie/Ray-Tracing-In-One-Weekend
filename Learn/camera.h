0350546fe640305e49e68273db561213fda69d0e
Richreggie <2326883616@qq.com>
2025-04-24 15:24:14 +0800

    漫反射实现

--- Learn/camera.h
+++ Learn/camera.h
@@ -94,12 +94,13 @@ private:
         hit_record rec;
 
         if (world.hit(r, interval(0, infinity), rec)) {
-            return 0.5 * (rec.normal + color(1, 1, 1));
+            vec3 direction = random_on_hemisphere(rec.normal);
+            return 0.5 * ray_color(ray(rec.p, direction), world);//漫反射球体
         }
 
         vec3 unit_direction = unit_vector(r.direction());
         auto a = 0.5 * (unit_direction.y() + 1.0);
-        return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+        return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);//渐变蓝天背景
     }
 };
 
--- Learn/vec3.h
+++ Learn/vec3.h
@@ -1,9 +1,10 @@
-#pragma once
+ï»¿#pragma once
 #ifndef VEC3_H
 #define VEC3_H
 
 #include <cmath>
 #include <iostream>
+#include "rtweekend.h"
 
 using std::sqrt;
 
@@ -47,13 +48,22 @@ public:
     double length_squared() const {
         return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
     }
+
+    static vec3 random() {
+        return vec3(random_double(), random_double(), random_double());
+    }
+
+    static vec3 random(double min, double max) {
+        return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+    }
+
 };
 
-// point3Ö»ÊÇvec3µÄ±ğÃû£¬µ«ÔÚ´úÂëÖĞÓÃÓÚ¼¸ºÎÇåÎú¶È¡£
+// point3åªæ˜¯vec3çš„åˆ«åï¼Œä½†åœ¨ä»£ç ä¸­ç”¨äºå‡ ä½•æ¸…æ™°åº¦ã€‚
 using point3 = vec3;
 
 
-// ÏòÁ¿ÊµÓÃº¯Êı
+// å‘é‡å®ç”¨å‡½æ•°
 
 inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
@@ -97,6 +107,26 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 
 inline vec3 unit_vector(const vec3& v) {
     return v / v.length();
+}
+
+inline vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1, 1);
+        if (p.length_squared() < 1)
+            return p;
+    }
+}
+
+inline vec3 random_unit_vector() {
+        return unit_vector(random_in_unit_sphere());
+    }
+
+inline vec3 random_on_hemisphere(const vec3& normal) {
+    vec3 on_unit_sphere = random_unit_vector();
+    if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+        return on_unit_sphere;
+    else
+        return -on_unit_sphere;
 }
 
 #endif
